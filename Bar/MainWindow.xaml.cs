10cf86032a9b992bb970e9fc6e57923f52eb77aa
Maksim Grushin <grusha-as@yandex.ru>
2025-02-18 17:27:42 +0500

    Added files to repository

---
+++ Bar/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowState = WindowState.Maximized;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            placeholder.Visibility = string.IsNullOrWhiteSpace(textBox1.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        private void textBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            placeholder2.Visibility = string.IsNullOrWhiteSpace(textBox2.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void textBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            placeholder3.Visibility = string.IsNullOrWhiteSpace(textBox3.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void textBox4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            placeholder4.Visibility = string.IsNullOrWhiteSpace(textBox4.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            MenuWindow menuWindow = new MenuWindow();
+            menuWindow.WindowState = WindowState.Maximized;
+            menuWindow.Show();
+
+            Close();
+        }
+    }
+}
---
+++ Bar/Models/Models.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bar.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Client> clients { get; set; } = null!;
+        public DbSet<Employee> employees { get; set; } = null!;
+        public DbSet<Drink> drinks { get; set; } = null!;
+        public DbSet<Table> tables { get; set; } = null!;
+        public DbSet<Order> orders { get; set; } = null!;
+        public DbSet<OrderItem> orderItems { get; set; } = null!;
+        public DbSet<Event> events { get; set; } = null!;
+        public DbSet<Reviews> reviews { get; set; } = null!;
+    }
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? phoneNumber { get; set; }
+        public DateTime visitDate { get; set; }
+        public string? preferences { get; set; }
+        public int visitCount { get; set; }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public string? membership { get; set; }
+        public string? post { get; set; }
+        public DateTime dateOfEmployment { get; set; }
+        public double pay { get; set; }
+    }
+
+    public class Drink
+    {
+        public int Id { get; set; }
+        public string? title { get; set; }
+        public bool isAlcohol { get; set; }
+        public double price { get; set; }
+        public double volume { get; set; }
+        public double alcoholDegree { get; set;}
+    }
+
+    public class Table
+    {
+        public int Id { get; set; }
+        public int tableNumber { get; set; }
+        public int capacity { get; set; }
+        public string? status { get; set; }
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int idTable { get; set; }
+        public Table? table { get; set; }
+
+        public double totalPrice { get; set; }
+        public DateTime datetime { get; set; }
+        public string? status { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public int Id { get; set; }
+
+        public int idOrder { get; set; }
+        public Order? order { get; set; }
+        public int idDrink { get; set; }
+        public Drink? drink { get; set; }
+
+        public int quantity { get; set; }
+        public double priceUnit { get; set; }
+    }
+
+    public class Event
+    {
+        public int Id { get; set; }
+        public string? eventName { get; set; }
+        public DateTime datetime { get; set; }
+        public string? description { get; set; }
+        public int maxNumberParticipants { get; set; }
+    }
+
+    public class Reviews
+    {
+        public int Id { get; set; }
+        
+        public int idClient { get; set; }
+        public Client? client { get; set; }
+
+        public int score { get; set; }
+        public string? textReview { get; set; }
+        public DateTime dateReview { get; set; }
+    }
+}
