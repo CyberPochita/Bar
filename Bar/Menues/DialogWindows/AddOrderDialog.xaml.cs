6498bd7d79a7c9efb6b46d04326ed3566d70cdef
Maksim Grushin <grusha-as@yandex.ru>
2025-02-19 19:39:58 +0500

    Создано диалоговое окно, в разработке. Проект обновлен

---
+++ Bar/Menues/DialogWindows/AddOrderDialog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Bar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bar.Menues.DialogWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrderDialog.xaml
+    /// </summary>
+    public partial class AddOrderDialog : Window
+    {
+        public AddOrderDialog(DatabaseModule db)
+        {
+            InitializeComponent();
+            Drinks drinks = new Drinks();
+            ContainerDrinks.Child = drinks;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DoneButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NumbersTableInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            placeholderNumbersTable.Visibility = string.IsNullOrWhiteSpace(NumbersTableInput.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
