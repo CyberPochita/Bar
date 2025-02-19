3e9106b7cb147eb9070442cce56a11d12fdc7ffe
Maksim Grushin <grusha-as@yandex.ru>
2025-02-19 19:34:38 +0500

    Убрал нужный код для Orders.xaml.cs из Clients.xaml.cs. А также добавил в Orders.xaml.cs Get запрос для таблицы Orders, чтобы поместить данные на верстку

--- Bar/Menues/Clients.xaml.cs
+++ Bar/Menues/Clients.xaml.cs
@@ -16,43 +16,12 @@ using System.Windows.Shapes;
 
 namespace Bar.Menues
 {
-    /// <summary>
-    /// Логика взаимодействия для Clients.xaml
-    /// </summary>
-    /// 
-    public class Card
-    {
-        public int id { get; set; }
-        public int idTable { get; set; }
-        public DateTime dateOrder { get; set; }
-        public double price { get; set; }
-        public string status { get; set; }
-    }
-
     public partial class Clients : UserControl
     {
-        public ObservableCollection<Card> cards { get; set; }
-
         public Clients()
         {
             InitializeComponent();
-            cards = new ObservableCollection<Card>();
-            DataContext = this;
-
-            for (int i = 0; i < 100; i++)
-            {
-                cards.Add(new Card { id = i + 1, idTable = i + 2, dateOrder = DateTime.Now, price = 123 + i * 2, status = "в обработке"});
-            }
-        }
-
-        private void AddButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void EditButton_Click(object sender, RoutedEventArgs e)
-        {
-
+            
         }
     }
 }
--- Bar/Menues/Orders.xaml.cs
+++ Bar/Menues/Orders.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Bar.Menues.DialogWindows;
+using Bar.Models;
+using Bogus;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +19,54 @@ using System.Windows.Shapes;
 
 namespace Bar.Menues
 {
-    /// <summary>
-    /// Логика взаимодействия для Orders.xaml
-    /// </summary>
+    public class Card
+    {
+        public int id { get; set; }
+        public int idTable { get; set; }
+        public DateTime dateOrder { get; set; }
+        public double price { get; set; }
+        public string status { get; set; }
+    }
+
     public partial class Orders : UserControl
     {
-        public Orders()
+        public ObservableCollection<Card> cards { get; set; }
+        public DatabaseModule db;
+
+        public Orders(DatabaseModule db)
         {
             InitializeComponent();
+            this.db = db;
+            cards = new ObservableCollection<Card>();
+            DataContext = this;
+
+            List<Order> orders = db.GetOrders();
+
+            foreach(Order order in orders)
+            {
+                cards.Add(new Card { id = order.Id, idTable = order.idTable, dateOrder = order.datetime, price = order.totalPrice, status = order.status });
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddOrderDialog addOrder = new AddOrderDialog(db);
+            addOrder.Show();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FreshOrders_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
