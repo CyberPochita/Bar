b4a754c13278954a6c637ba5bb37e82408535d7f
Maksim Grushin <grusha-as@yandex.ru>
2025-02-20 19:41:17 +0500

    Добавлена визуальная функция - цвет контейнера напитка меняется на 'выбранный' цвет и обратно

--- Bar/Menues/Drinks.xaml.cs
+++ Bar/Menues/Drinks.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using Bar.Models;
-using Bogus;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +31,9 @@ namespace Bar.Menues
     public partial class Drinks : UserControl
     {
         public ObservableCollection<CardDrink> cards { get; set; }
+        private int clickCount = 0;
+        private string originalColor = "#242424";
+        private string selectedColor = "#2A3527";
 
         public Drinks(DatabaseModule db)
         {
@@ -42,6 +45,33 @@ namespace Bar.Menues
             foreach(var drink in drinks)
             {
                 cards.Add(new CardDrink { Id = drink.Id, title = drink.title, isAlcohol = drink.isAlcohol, price = drink.price, volume = drink.volume, degree = drink.alcoholDegree});
+            }
+        }
+
+        private void ContainerBorder_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                clickCount++;
+
+                var timer = new System.Timers.Timer(300);
+                timer.Elapsed += (s, args) =>
+                {
+                    clickCount = 0;
+                    timer.Stop();
+                    timer.Dispose();
+                };
+                timer.Start();
+
+                if(clickCount == 2)
+                {
+                    var border = sender as Border;
+
+                    if(border.Background.ToString() == new SolidColorBrush((Color)ColorConverter.ConvertFromString(originalColor)).ToString())
+                        border.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(selectedColor));
+                    else
+                        border.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(originalColor));
+                }
             }
         }
     }
