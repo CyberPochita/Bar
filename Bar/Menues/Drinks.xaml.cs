2942f29bf734c1313950cc9abf8e525ab9063d74
Maksim Grushin <grusha-as@yandex.ru>
2025-02-20 20:05:23 +0500

    Добавил метод, который возвращает выбранные напитки

--- Bar/Menues/Drinks.xaml.cs
+++ Bar/Menues/Drinks.xaml.cs
@@ -34,11 +34,14 @@ namespace Bar.Menues
         private int clickCount = 0;
         private string originalColor = "#242424";
         private string selectedColor = "#2A3527";
+        private List<string> selectedDrinks;
+        private DatabaseModule db;
 
         public Drinks(DatabaseModule db)
         {
             InitializeComponent();
             cards = new ObservableCollection<CardDrink>();
+            this.db = db;
             List<Drink> drinks = db.GetDrinks();
             DataContext = this;
 
@@ -46,6 +49,11 @@ namespace Bar.Menues
             {
                 cards.Add(new CardDrink { Id = drink.Id, title = drink.title, isAlcohol = drink.isAlcohol, price = drink.price, volume = drink.volume, degree = drink.alcoholDegree});
             }
+        }
+
+        public List<Drink> GetSelectedDrinks()
+        {
+            return new List<Drink>(db.GetDrinksByTitles(selectedDrinks));
         }
 
         private void ContainerBorder_MouseDown(object sender, MouseButtonEventArgs e)
@@ -67,10 +75,34 @@ namespace Bar.Menues
                 {
                     var border = sender as Border;
 
-                    if(border.Background.ToString() == new SolidColorBrush((Color)ColorConverter.ConvertFromString(originalColor)).ToString())
+                    if (border.Background.ToString() == new SolidColorBrush((Color)ColorConverter.ConvertFromString(originalColor)).ToString())
+                    {
                         border.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(selectedColor));
+                        var cardDrink = border.DataContext as CardDrink;
+
+                        if(cardDrink != null)
+                        {
+                            string title = cardDrink.title;
+                            selectedDrinks.Add(title);
+                        }
+                    }
                     else
+                    {
                         border.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(originalColor));
+
+                        var cardDrink = border.DataContext as CardDrink;
+                        if (cardDrink != null)
+                        {
+                            foreach (var item in selectedDrinks)
+                            {
+                                if (cardDrink.title == item)
+                                {
+                                    selectedDrinks.Remove(item);
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
