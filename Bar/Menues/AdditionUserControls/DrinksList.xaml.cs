326efde050bc4a0b6d9905e7c6fc9cbf63816764
CyberPochita <maksimgrusin963@gmail.com>
2025-05-19 17:47:17 +0500

    Реализовал поиск напитков и фильтрацию по параметру

--- Bar/Menues/AdditionUserControls/DrinksList.xaml.cs
+++ Bar/Menues/AdditionUserControls/DrinksList.xaml.cs
@@ -2,18 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bar.Menues.AdditionUserControls
 {
@@ -30,6 +21,7 @@ namespace Bar.Menues.AdditionUserControls
     public partial class DrinksList : UserControl
     {
         public ObservableCollection<CardDrink> cards { get; set; }
+        private List<CardDrink> allDrinks;
         private int clickCount = 0;
         private string originalColor = "#242424";
         private string selectedColor = "#2A3527";
@@ -41,13 +33,102 @@ namespace Bar.Menues.AdditionUserControls
             InitializeComponent();
             cards = new ObservableCollection<CardDrink>();
             this.db = db;
-            List<Drink> drinks = db.GetDrinks();
+            allDrinks = new List<CardDrink>();
             selectedDrinks = new List<string>();
             DataContext = this;
 
+            LoadDrinks();
+        }
+
+        private void LoadDrinks()
+        {
+            List<Drink> drinks = db.GetDrinks();
+            cards.Clear();
+            allDrinks.Clear();
+
             foreach (var drink in drinks)
             {
-                cards.Add(new CardDrink { Id = drink.Id, title = drink.title, isAlcohol = drink.isAlcohol, price = drink.price, volume = drink.volume, degree = drink.alcoholDegree });
+                var card = new CardDrink
+                {
+                    Id = drink.Id,
+                    title = drink.title,
+                    isAlcohol = drink.isAlcohol,
+                    price = drink.price,
+                    volume = drink.volume,
+                    degree = drink.alcoholDegree
+                };
+
+                cards.Add(card);
+                allDrinks.Add(card);
+            }
+        }
+
+        public void SubstringSearch(string word)
+        {
+            if (string.IsNullOrWhiteSpace(word))
+            {
+                
+                cards.Clear();
+                foreach (var drink in allDrinks)
+                {
+                    cards.Add(drink);
+                }
+                return;
+            }
+
+            word = word.ToLower();
+            
+            var resultDrinks = allDrinks
+                .Where(drink => drink.title != null && drink.title.ToLower().Contains(word))
+                .OrderBy(drink => drink.title)
+                .ToList();
+
+            cards.Clear();
+            foreach (var drink in resultDrinks)
+            {
+                cards.Add(drink);
+            }
+        }
+
+        public void ApplyFilter(string filter)
+        {
+            // Создаем новую коллекцию для результатов
+            var filteredCollection = new ObservableCollection<CardDrink>();
+
+            // Применяем фильтр
+            switch (filter)
+            {
+                case "Is alcohol":
+                    foreach (var drink in allDrinks.Where(d => d.isAlcohol))
+                        filteredCollection.Add(drink);
+                    break;
+
+                case "By price":
+                    foreach (var drink in allDrinks.OrderBy(d => d.price))
+                        filteredCollection.Add(drink);
+                    break;
+
+                case "By volume":
+                    foreach (var drink in allDrinks.OrderBy(d => d.volume))
+                        filteredCollection.Add(drink);
+                    break;
+
+                case "By alcohol degree":
+                    foreach (var drink in allDrinks.Where(d => d.isAlcohol).OrderBy(d => d.degree))
+                        filteredCollection.Add(drink);
+                    break;
+
+                default: // "All drinks"
+                    foreach (var drink in allDrinks)
+                        filteredCollection.Add(drink);
+                    break;
+            }
+
+            // Полностью заменяем коллекцию
+            cards.Clear();
+            foreach (var item in filteredCollection)
+            {
+                cards.Add(item);
             }
         }
 
--- Bar/Menues/Drinks.xaml.cs
+++ Bar/Menues/Drinks.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Bar.Models;
+﻿using Bar.Menues.AdditionUserControls;
+using Bar.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -18,93 +19,36 @@ using System.Windows.Shapes;
 
 namespace Bar.Menues
 {
-    public class CardDrink
-    {
-        public int Id { get; set; }
-        public string? title { get; set; }
-        public bool isAlcohol { get; set; }
-        public double price { get; set; }
-        public double volume { get; set; }
-        public double degree { get; set; }
-    }
-
     public partial class Drinks : UserControl
     {
-        public ObservableCollection<CardDrink> cards { get; set; }
-        private int clickCount = 0;
-        private string originalColor = "#242424";
-        private string selectedColor = "#2A3527";
-        private List<string>? selectedDrinks;
         private DatabaseModule db;
-
+        private DrinksList drinksList;
         public Drinks(DatabaseModule db)
         {
             InitializeComponent();
-            cards = new ObservableCollection<CardDrink>();
             this.db = db;
-            List<Drink> drinks = db.GetDrinks();
-            selectedDrinks = new List<string>();
-            DataContext = this;
-
-            foreach(var drink in drinks)
-            {
-                cards.Add(new CardDrink { Id = drink.Id, title = drink.title, isAlcohol = drink.isAlcohol, price = drink.price, volume = drink.volume, degree = drink.alcoholDegree});
-            }
+            drinksList = new(db);
+            GridDrinks.Children.Add(drinksList);
         }
 
-        public List<Drink> GetSelectedDrinks()
+        private void NameDrink_TextChanged(object sender, TextChangedEventArgs e)
         {
-            return new List<Drink>(db.GetDrinksByTitles(selectedDrinks));
+            placeholderNameDrink.Visibility = string.IsNullOrWhiteSpace(NameDrink.Text) ? Visibility.Visible : Visibility.Collapsed;
         }
 
-        private void ContainerBorder_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Search_Click(object sender, RoutedEventArgs e)
         {
-            if(e.ChangedButton == MouseButton.Left)
+            string word = NameDrink.Text.Trim();
+
+            drinksList.SubstringSearch(word);
+        }
+
+        private void ComboFiltr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboFiltr.SelectedItem is ComboBoxItem selectedItem)
             {
-                clickCount++;
-
-                var timer = new System.Timers.Timer(300);
-                timer.Elapsed += (s, args) =>
-                {
-                    clickCount = 0;
-                    timer.Stop();
-                    timer.Dispose();
-                };
-                timer.Start();
-
-                if(clickCount == 2)
-                {
-                    var border = sender as Border;
-
-                    if (border.Background.ToString() == new SolidColorBrush((Color)ColorConverter.ConvertFromString(originalColor)).ToString())
-                    {
-                        border.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(selectedColor));
-                        var cardDrink = border.DataContext as CardDrink;
-
-                        if(cardDrink != null)
-                        {
-                            string title = cardDrink.title;
-                            selectedDrinks.Add(title);
-                        }
-                    }
-                    else
-                    {
-                        border.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(originalColor));
-
-                        var cardDrink = border.DataContext as CardDrink;
-                        if (cardDrink != null)
-                        {
-                            foreach (var item in selectedDrinks)
-                            {
-                                if (cardDrink.title == item)
-                                {
-                                    selectedDrinks.Remove(item);
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
+                string filter = selectedItem.Content.ToString();
+                drinksList.ApplyFilter(filter);
             }
         }
     }
